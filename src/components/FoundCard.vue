640a9cb59a9bbf3d9563895b1159cf19be399b5c
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-18 11:26:22 +0200

    Inserire api db.ygoprodeck e personalizzare stile

--- src/components/FoundCard.vue
+++ src/components/FoundCard.vue
@@ -1,5 +1,51 @@
+<script setup>
+
+import axios from 'axios';
+
+</script>
+
 <script>
 
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+    data() {
+        return {
+            cards: [],
+        }
+    },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+    methods: {
+        getApiYugioh() {
+            // Make a request for a user with a given ID
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+            .then((response) => {
+                // handle success
+                console.log(response);
+                this.cards = response.data.data
+            })
+            .catch((error) => {
+                // handle error
+                console.log(error);
+            })
+            .finally(() => {
+                // always executed
+            });
+        }
+    },
+    created() {
+        this.getApiYugioh()
+    },
+
+    // Lifecycle hooks are called at different stages
+    // of a component's lifecycle.
+    // This function will be called when the component is mounted.
+    mounted() {
+        console.log(this.cards.length)
+    }
+}
 </script>
 
 <template>
@@ -8,8 +54,16 @@
 
         <aside>
             <div>
-                <h2>Found 39 Cards</h2>
-                <span></span>
+                <h2>Found {{ cards.length }} Cards</h2>
+            </div>
+            <div>
+                <ul v-for="(cards, index) in cards" :key="index">
+                    <li>
+                        <img :src="cards.card_images[0].image_url" :alt="cards.name">
+                    </li>
+                    <li> {{ cards.name }} </li>
+                    <li> {{ cards.archetype }} </li>
+                </ul>
             </div>
         </aside>
 
@@ -22,9 +76,12 @@
 @use '../styles/partials/variants' as *;
 
 aside{
+    padding: 2.5rem;
+    background-color: white;
+
     div{
+        width: 100%;
         background-color: $color-white;
-        padding: 2.5rem 2.5rem 0 2.5rem;
 
         h2{
             color: $color-white;
@@ -33,13 +90,38 @@ aside{
             font-size: .7rem;
         }
 
-        span{
-            display: block;
-            height: 42.8em;
+
+    }
+
+    div:nth-of-type(2) {
+        @include d-flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+
+        ul{
+            width: calc((100% / 5) - 1rem);
             margin-bottom: 1rem;
-            background-color: red;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            background-color: #d48f38;
+            li {
+                list-style: none;
+                margin-bottom: .3rem;
+                text-align: center;
+                color: white;
+
+                img{
+                    width: -moz-available;
+                }
+            }
+
+            li:nth-of-type(2) {
+                text-transform: uppercase;
+                font-size: .8rem;
+                font-weight: 900;
+            }
         }
     }
 }
-
 </style>
